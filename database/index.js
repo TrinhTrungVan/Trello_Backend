ce92fd1d9cf59789c3d7329796d46bb7388458e3
TrungVan <vantrinhtrung@gmail.com>
2022-07-30 14:06:57 +0700

    Test deploy

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/trello_clone");
+        await mongoose.connect("mongodb+srv://trungvan1904:trungvan1904@cluster0.ozaqi.mongodb.net/test");
         console.log("Connect successfully!");
     } catch (error) {
         console.log(error);
