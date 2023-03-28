606150ba0fe6a75f721992939d98cd47f58e5f49
TrinhTrungVan <vantrinhtrung@gmail.com>
2023-03-28 22:25:08 +0700

    Edit Mongo uri

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/trello_clone");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connect successfully!");
     } catch (error) {
         console.log(error);
