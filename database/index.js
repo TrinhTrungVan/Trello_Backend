eeab468526e0e33beab3c3f4a62233d6c8c78718
TrinhTrungVan <vantrinhtrung@gmail.com>
2023-03-28 22:37:57 +0700

    Add file vercel.json

--- database/index.js
+++ database/index.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(
+            process.env.MONGO_URI ||
+                "mongodb+srv://TrungVan1904:TrungVan1904@mycluster.96efpsw.mongodb.net/Trello_Clone"
+        );
         console.log("Connect successfully!");
     } catch (error) {
         console.log(error);
